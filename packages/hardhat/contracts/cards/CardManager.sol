50d1c15802f4dc8213412654055b0e4336001286
Xavier Damman <xdamman@gmail.com>
2024-04-10 23:08:50 +0200

    Update smartcontracts to get ABI

--- packages/hardhat/contracts/cards/CardManager.sol
+++ packages/hardhat/contracts/cards/CardManager.sol
@@ -8,6 +8,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
 
 import "./Card.sol";
+import "./interfaces/IOwnable.sol";
 import "./interfaces/ITokenEntryPoint.sol";
 import "./interfaces/IWhitelist.sol";
 import "./interfaces/IWithdrawable.sol";
@@ -17,134 +18,157 @@ import "./interfaces/IWithdrawable.sol";
  * @dev A factory contract that creates Card contracts.
  */
 contract CardManager is IWhitelist {
-    Card public immutable cardImplementation;
+	Card public immutable cardImplementation;
 
-    event CardCreated(address indexed voucher);
+	event CardCreated(address indexed voucher);
 
-    constructor(
-        IEntryPoint _entryPoint,
-        ITokenEntryPoint _tokenEntryPoint,
-        address[] memory _whitelistAddresses
-    ) {
-        cardImplementation = new Card(_entryPoint, _tokenEntryPoint);
-        _whitelistVersion = 0;
-        _updateWhiteList(_whitelistAddresses);
-        owner = msg.sender;
-    }
+	constructor(
+		IEntryPoint _entryPoint,
+		ITokenEntryPoint _tokenEntryPoint,
+		address[] memory _whitelistAddresses
+	) {
+		cardImplementation = new Card(_entryPoint, _tokenEntryPoint);
+		_whitelistVersion = 0;
+		_updateWhiteList(_whitelistAddresses);
+		owner = msg.sender;
+	}
 
-    /**
-     * @dev Calculates the hash value for a given card.
-     * This function should only be used to test hash values.
-     *
-     * @param serial The serial to be hashed.
-     * @return The calculated hash value.
-     */
-    function getCardHash(uint256 serial) public view returns (bytes32) {
-        return
-            keccak256(abi.encodePacked(serial, block.chainid, address(this)));
-    }
+	/**
+	 * @dev Calculates the hash value for a given card.
+	 * This function should only be used to test hash values.
+	 *
+	 * @param serial The serial to be hashed.
+	 * @return The calculated hash value.
+	 */
+	function getCardHash(uint256 serial) public view returns (bytes32) {
+		return
+			keccak256(abi.encodePacked(serial, block.chainid, address(this)));
+	}
 
-    function createCard(bytes32 cardHash) public returns (Card ret) {
-        address addr = getCardAddress(cardHash);
+	function createCard(bytes32 cardHash) public returns (Card ret) {
+		address addr = getCardAddress(cardHash);
 
-        emit CardCreated(addr);
+		emit CardCreated(addr);
 
-        uint codeSize = addr.code.length;
-        if (codeSize > 0) {
-            return Card(payable(addr));
-        }
-        ret = Card(
-            payable(
-                new ERC1967Proxy{salt: cardHash}(
-                    address(cardImplementation),
-                    abi.encodeCall(Card.initialize, ((this)))
-                )
-            )
-        );
+		uint codeSize = addr.code.length;
+		if (codeSize > 0) {
+			return Card(payable(addr));
+		}
+		ret = Card(
+			payable(
+				new ERC1967Proxy{ salt: cardHash }(
+					address(cardImplementation),
+					abi.encodeCall(Card.initialize, ((this)))
+				)
+			)
+		);
 
-        // transfer ownership to the card manager
-        ret.transferOwnership(address(this));
-    }
+		// transfer ownership to the card manager
+		ret.transferOwnership(address(this));
+	}
 
-    /**
-     * calculate the counterfactual address of this card as it would be returned by createCard()
-     */
-    function getCardAddress(bytes32 codeHash) public view returns (address) {
-        return
-            Create2.computeAddress(
-                codeHash,
-                keccak256(
-                    abi.encodePacked(
-                        type(ERC1967Proxy).creationCode,
-                        abi.encode(
-                            address(cardImplementation),
-                            abi.encodeCall(Card.initialize, ((this)))
-                        )
-                    )
-                )
-            );
-    }
+	/**
+	 * calculate the counterfactual address of this card as it would be returned by createCard()
+	 */
+	function getCardAddress(bytes32 cardHash) public view returns (address) {
+		return
+			Create2.computeAddress(
+				cardHash,
+				keccak256(
+					abi.encodePacked(
+						type(ERC1967Proxy).creationCode,
+						abi.encode(
+							address(cardImplementation),
+							abi.encodeCall(Card.initialize, ((this)))
+						)
+					)
+				)
+			);
+	}
 
-    // helper function for withdrawing from Cards
-    function withdraw(
-        IWithdrawable card,
-        IERC20 token,
-        address to,
-        uint256 amount
-    ) public {
-        card.withdrawTo(token, to, amount);
-    }
+	// helper function for withdrawing from Cards and creating the card if needed
+	function withdraw(
+		bytes32 cardHash,
+		IERC20 token,
+		address to,
+		uint256 amount
+	) public {
+		address cardAddress = getCardAddress(cardHash);
+		bool exists = contractExists(cardAddress);
+		if (!exists) {
+			createCard(cardHash);
+		}
 
-    // allow card ownership to be given away
-    function transferCardOwnership(
-        Card card,
-        address newOwner
-    ) public onlyOwner {
-        card.transferOwnership(newOwner);
-    }
+		IWithdrawable card = IWithdrawable(cardAddress);
 
-    // ownership management
-    address public owner;
+		card.withdrawTo(token, to, amount);
+	}
 
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        _checkOwner();
-        _;
-    }
+	// allow card ownership to be given away
+	function transferCardOwnership(bytes32 cardHash, address newOwner) public {
+		Card card = Card(
+			payable(
+				new ERC1967Proxy{ salt: cardHash }(
+					getCardAddress(cardHash),
+					abi.encodeCall(Card.initialize, ((this)))
+				)
+			)
+		);
+		card.transferOwnership(newOwner);
+	}
 
-    function _checkOwner() internal view virtual {
-        require(owner == msg.sender, "Ownable: caller is not the owner");
-    }
+	// ownership management
+	address public owner;
 
-    // more gas efficient for updating the whitelist than only using a mapping
-    uint256 private _whitelistVersion = 0;
-    mapping(address => uint256) private _whitelist;
+	/**
+	 * @dev Throws if called by any account other than the owner.
+	 */
+	modifier onlyOwner() {
+		_checkOwner();
+		_;
+	}
 
-    /**
-     * @dev Checks if an address is in the whitelist.
-     * @param addr The address to check.
-     * @return A boolean indicating whether the address is in the whitelist.
-     */
-    function isWhitelisted(address addr) external view returns (bool) {
-        return _whitelist[addr] == _whitelistVersion;
-    }
+	function _checkOwner() internal view virtual {
+		require(owner == msg.sender, "Ownable: caller is not the owner");
+	}
 
-    function _updateWhiteList(address[] memory addresses) internal virtual {
-        // bump the version number so that we don't have to clear the mapping
-        _whitelistVersion++;
+	// more gas efficient for updating the whitelist than only using a mapping
+	uint256 private _whitelistVersion = 0;
+	mapping(address => uint256) private _whitelist;
 
-        for (uint i = 0; i < addresses.length; i++) {
-            _whitelist[addresses[i]] = _whitelistVersion;
-        }
-    }
+	/**
+	 * @dev Checks if an address is in the whitelist.
+	 * @param addr The address to check.
+	 * @return A boolean indicating whether the address is in the whitelist.
+	 */
+	function isWhitelisted(address addr) external view returns (bool) {
+		return _whitelist[addr] == _whitelistVersion;
+	}
 
-    /**
-     * @dev Updates the whitelist.
-     * @param addresses The addresses to update the whitelist.
-     */
-    function updateWhitelist(address[] memory addresses) public onlyOwner {
-        _updateWhiteList(addresses);
-    }
+	function _updateWhiteList(address[] memory addresses) internal virtual {
+		// bump the version number so that we don't have to clear the mapping
+		_whitelistVersion++;
+
+		for (uint i = 0; i < addresses.length; i++) {
+			_whitelist[addresses[i]] = _whitelistVersion;
+		}
+	}
+
+	/**
+	 * @dev Updates the whitelist.
+	 * @param addresses The addresses to update the whitelist.
+	 */
+	function updateWhitelist(address[] memory addresses) public onlyOwner {
+		_updateWhiteList(addresses);
+	}
+
+	function contractExists(
+		address contractAddress
+	) public view returns (bool) {
+		uint size;
+		assembly {
+			size := extcodesize(contractAddress)
+		}
+		return size > 0;
+	}
 }
