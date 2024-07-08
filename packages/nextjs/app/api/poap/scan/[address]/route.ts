9ee716182b9f3ff7940b85df366b22c7607f9c1e
Xavier Damman <xdamman@gmail.com>
2024-07-08 03:07:02 +0200

    debugging

--- packages/nextjs/app/api/poap/scan/[address]/route.ts
+++ packages/nextjs/app/api/poap/scan/[address]/route.ts
@@ -9,14 +9,17 @@ type paramsType = {
 export async function GET(request: NextRequest, { params }: { params: paramsType }) {
   const apiKey = process.env.POAP_API_KEY || "";
   const limit = request.nextUrl.searchParams.get("limit");
-
+  console.log(">>> GET /api/poap/scan/[address]", params.address, limit, apiKey);
   const response = await fetch(`https://api.poap.tech/actions/scan/${params.address}`, {
     headers: { "x-api-key": apiKey },
   });
+  console.log(">>> headers", { "x-api-key": apiKey });
   const data = await response.json();
   if (limit && Number(limit) > 0) {
+    console.log(">>> Returning limited data", data.length, limit, data);
     return Response.json(data.slice(0, Number(limit)));
   } else {
+    console.log(">>> Returning data", data.length, data);
     return Response.json(data);
   }
 }
